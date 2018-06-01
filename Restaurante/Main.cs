9686dba8f96bd718f451f4912cf602f207856b6e
gemalo28 <gemalo28@gmail.com>
2018-05-31 23:11:29 -0600

    Add files via upload

---
+++ Restaurante/Main.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data;
+using MySql.Data.MySqlClient;
+using ReglasDelNegocio;
+
+namespace Restaurante
+{
+    public partial class Main : Form
+    {
+        private MySqlConnection xConnection = new MySqlConnection();
+        public Main()
+        {
+            InitializeComponent();            
+        }
+    }
+}
