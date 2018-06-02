77dc830f180447b15801e3f593c1e38bcdb605aa
Carlos <Carlos.gutierrezlugo@gmail.com>
2018-06-01 23:01:01 -0600

    PRIMERA CREACIOND DE PANTALLA

---
+++ Restaurante/logIn.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace Restaurante
+{
+    public partial class logIn : Form
+    {
+        
+        
+       
+        public logIn()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEntrar_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
