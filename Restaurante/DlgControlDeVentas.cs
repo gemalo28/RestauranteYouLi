675be46c44169fa0d6e6cf79cd4d0cfc4fe04d4e
gemalo28 <gemalo28@gmail.com>
2018-06-17 01:59:19 -0600

    Update DlgControlDeVentas.cs

--- Restaurante/DlgControlDeVentas.cs
+++ Restaurante/DlgControlDeVentas.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -20,58 +20,25 @@ namespace Restaurante
         public DlgControlDeVentas(MySqlConnection xConnection)
         {
             InitializeComponent();
-            this.xConnection = xConnection;
-            xNotas = new Notas(xConnection);
-            dateTimePicker1.Value = DateTime.Today;
-            dateTimePicker2.Value = DateTime.Today;
-        
+            this.xConnection = xConnection;        
         }
 
-        private void DlgControlDeVentas_Load(object sender, EventArgs e)
+        private void btnNotas_Click(object sender, EventArgs e)
         {
-            llenarNotas( xNotas.ConsultarNota());
+            DlgNotas dlgNotas = new DlgNotas(xConnection);
+            dlgNotas.ShowDialog();
         }
 
-        private void btnBuscar_Click(object sender, EventArgs e)
+        private void btnProductos_Click(object sender, EventArgs e)
         {
-            if(dateTimePicker1.Value.Date <= dateTimePicker2.Value.Date)
-            {
-                llenarNotas(xNotas.ConsultarNota(dateTimePicker1.Value, dateTimePicker2.Value));
-            }
-            else
-            {
-                MessageBox.Show("Favor de verificar las fechas seleccionadas...");
-            }
-        }
-        //select id_nota as ID, propietario as Propietario, date(fecha) as Fecha, descripcion as Descripcion, total as Total
-        public void llenarNotas(DataTable dtOrdenes)
-        {
-          //  if (dgvVentas.Rows.Count >0)
-            {
-                dgvVentas.Rows.Clear();
-
-
-            }
-            foreach (DataRow row in dtOrdenes.Rows)
-            {
-                dgvVentas.Rows.Add(row[0].ToString(), row[1].ToString(), row[2].ToString(), row[3].ToString(), row[4].ToString());
-            }
-
+            DlgReportesProductos dlgReportes = new DlgReportesProductos(xConnection);
+            dlgReportes.ShowDialog();
         }
 
-        private void dgvVentas_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        private void btnInventario_Click(object sender, EventArgs e)
         {
-
-
-        }
-
-        private void dgvVentas_CellDoubleClick_1(object sender, DataGridViewCellEventArgs e)
-        {
-            if (e.RowIndex >= 0)
-            {
-                DlgDetalleVenta dlgDetalle = new DlgDetalleVenta(xConnection, dgvVentas.Rows[e.RowIndex]);
-                dlgDetalle.ShowDialog();
-            }
+            DlgReporteInventario dlgInventario = new DlgReporteInventario(xConnection);
+            dlgInventario.ShowDialog();
         }
     }
 }
