76c673609ff9f391febafb7a0eb9a011d171c5c5
gemalo28 <gemalo28@gmail.com>
2018-06-16 21:42:16 -0600

    Update script you_li.sql

--- script you_li.sql
+++ script you_li.sql
@@ -200,12 +200,27 @@ Delimiter $$
 CREATE TRIGGER tr_Delete_Detalle 
 AFTER DELETE ON you_li.detalle_orden FOR EACH ROW
 BEGIN
-   update ordenes o, (select sum(p.precio) as Total
+   update ordenes o, (select coalesce((select sum(p.precio) 
 						from detalle_orden d
 						join productos p on p.id_producto = d.id_producto
-						where d.id_orden = old.id_orden) src
+						where d.id_orden = old.id_orden), 0 ) as Total) src
    set o.total = src.Total
    where o.id_orden = old.id_orden;
+END 
+$$
+
+Delimiter $$
+CREATE TRIGGER tr_Update_Detalle 
+AFTER UPDATE ON detalle_orden FOR EACH ROW
+BEGIN   
+   select coalesce((select count(flag_pagado) 
+   from detalle_orden
+   where id_orden = new.id_orden and flag_pagado = 0),0) into @nopagados;   
+   
+   if @nopagados = 0 then   
+   update ordenes set flag_pagado = 1
+   where id_orden = new.id_orden;
+   end if;
 END 
 $$
 
